f1435dc72722670f949e4da7e73aad30efe5b9d2
문영호 <yemoon@ebay.com>
2019-01-28 21:19:31 +0900

    add

--- IP.Bee/areas/errors/not-found.js
+++
@@ -1,8 +0,0 @@
-var join = require('path').join
-
-function notFound(req, res) {
-    res.status(404);
-    res.sendFile(join(__dirname + '/../static/noimage_600.png'));
-}
-
-module.exports = notFound
